17688df8a414062398d524d932eee83a832eff33
kemosabe <river@kemosabe.media>
2026-02-27 17:00:58 -0600

    Auto: apps/radiator/src/app/providers.tsx |  1 file changed, 18 insertions(+), 19 deletions(-)

--- apps/radiator/src/app/providers.tsx
+++ apps/radiator/src/app/providers.tsx
@@ -1,38 +1,36 @@
 'use client';
 
-import { useMemo } from "react";
-import { Toaster } from "react-hot-toast";
+import { useMemo } from 'react';
 import {
   ConnectionProvider,
   WalletProvider,
-} from "@solana/wallet-adapter-react";
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+} from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import {
   PhantomWalletAdapter,
-  SolflareWalletAdapter
-} from "@solana/wallet-adapter-wallets";
-import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { defaultRpcEndpoint } from "@/constants";
-import { ConnectionConfig } from "@solana/web3.js";
-require("@solana/wallet-adapter-react-ui/styles.css");
+  SolflareWalletAdapter,
+} from '@solana/wallet-adapter-wallets';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { ToastProvider } from '@rdna/radiants/components/core';
+import { defaultRpcEndpoint } from '@/constants';
+import { ConnectionConfig } from '@solana/web3.js';
 
-export function Providers({ children }: any) {
+export function Providers({ children }: { children: React.ReactNode }) {
   const endpoint = process.env.NEXT_PUBLIC_RPC_ENDPOINT || defaultRpcEndpoint;
-  console.log('endpoint: ', endpoint);
 
-  const network = endpoint.includes("devnet")
+  const network = endpoint.includes('devnet')
     ? WalletAdapterNetwork.Devnet
     : WalletAdapterNetwork.Mainnet;
 
   const connectionConfig: ConnectionConfig = {
-    commitment: "confirmed",
-    confirmTransactionInitialTimeout: 60000
+    commitment: 'confirmed',
+    confirmTransactionInitialTimeout: 60000,
   };
 
   const wallets = useMemo(
     () => [
       new PhantomWalletAdapter(),
-      new SolflareWalletAdapter({ network })
+      new SolflareWalletAdapter({ network }),
     ],
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [network]
@@ -42,8 +40,9 @@ export function Providers({ children }: any) {
     <ConnectionProvider endpoint={endpoint} config={connectionConfig}>
       <WalletProvider wallets={wallets} autoConnect>
         <WalletModalProvider>
-          {children}
-          <Toaster />
+          <ToastProvider>
+            {children}
+          </ToastProvider>
         </WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
