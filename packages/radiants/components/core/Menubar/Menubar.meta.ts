f6b95c58554deba162d4450dc9e078b7bec4847f
kemosabe <river@kemosabe.media>
2026-03-19 22:20:12 -0500

    Auto: packages/radiants/components/core/Menubar/Menubar.meta.ts |  1 file changed, 45 insertions(+)

---
+++ packages/radiants/components/core/Menubar/Menubar.meta.ts
@@ -0,0 +1,45 @@
+import { defineComponentMeta } from "@rdna/preview/define-component-meta";
+
+interface MenubarProps {
+  modal?: boolean;
+  orientation?: string;
+}
+
+export const MenubarMeta = defineComponentMeta<MenubarProps>()({
+  name: "Menubar",
+  description:
+    "Desktop-style application menu bar with keyboard navigation and nested menus.",
+  subcomponents: [
+    "Menubar.Root",
+    "Menubar.Menu",
+    "Menubar.Trigger",
+    "Menubar.Content",
+    "Menubar.Item",
+    "Menubar.Separator",
+    "Menubar.Label",
+  ],
+  props: {
+    modal: {
+      type: "boolean",
+      default: false,
+      description: "Whether menu traps focus (modal mode)",
+    },
+    orientation: {
+      type: "string",
+      default: "horizontal",
+      description: "Orientation of the menubar",
+    },
+  },
+  slots: {},
+  examples: [
+    {
+      name: "Basic menubar",
+      code: '<Menubar.Root>\n  <Menubar.Menu>\n    <Menubar.Trigger>File</Menubar.Trigger>\n    <Menubar.Content>\n      <Menubar.Item>New</Menubar.Item>\n      <Menubar.Item>Open</Menubar.Item>\n      <Menubar.Separator />\n      <Menubar.Item>Save</Menubar.Item>\n    </Menubar.Content>\n  </Menubar.Menu>\n</Menubar.Root>',
+    },
+  ],
+  registry: {
+    category: "navigation",
+    tags: ["menubar", "menu", "desktop", "file-menu"],
+    renderMode: "custom",
+  },
+});
