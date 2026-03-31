9878f36abe3c7d6d559a1eb04fe2d080bc5c4fb1
kemosabe <river@kemosabe.media>
2026-03-31 16:02:51 -0600

    Auto: packages/radiants/blocknote/renders/Pattern.tsx |  1 file changed, 10 insertions(+)

---
+++ packages/radiants/blocknote/renders/Pattern.tsx
@@ -0,0 +1,10 @@
+import { Pattern } from '../../components/core';
+import type { BlockNoteRenderProps } from '../types';
+
+export function renderPatternBlock({ contentRef }: BlockNoteRenderProps) {
+  return (
+    <Pattern pat="diagonal-dots" bg className="p-4 my-1">
+      <span ref={contentRef} />
+    </Pattern>
+  );
+}
