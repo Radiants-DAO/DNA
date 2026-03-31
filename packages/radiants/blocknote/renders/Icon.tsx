45a8c96f750bb6d6896e7f91934973dd223eb2c2
kemosabe <river@kemosabe.media>
2026-03-31 16:04:02 -0600

    Auto: packages/radiants/blocknote/renders/Icon.tsx |  1 file changed, 6 insertions(+)

---
+++ packages/radiants/blocknote/renders/Icon.tsx
@@ -0,0 +1,6 @@
+import { Icon } from '../../icons/runtime';
+import type { BlockNoteRenderProps } from '../types';
+
+export function renderIconBlock(_props: BlockNoteRenderProps) {
+  return <Icon name="plus" large />;
+}
