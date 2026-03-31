ffc3d902619bc8270308bb27f8b7caeae845ab19
kemosabe <river@kemosabe.media>
2026-03-31 16:00:37 -0600

    Auto: packages/radiants/blocknote/renders/ContextMenu.tsx |  1 file changed, 9 insertions(+)

---
+++ packages/radiants/blocknote/renders/ContextMenu.tsx
@@ -0,0 +1,9 @@
+import type { BlockNoteRenderProps } from '../types';
+
+export function renderContextMenuBlock(_props: BlockNoteRenderProps) {
+  return (
+    <div className="py-2 px-3 border border-dashed border-line text-mute text-sm font-joystix my-1">
+      Context Menu
+    </div>
+  );
+}
